6dd425fb82778695f61e9c441e025f12bae18d27
Colman <colmanseery@gmail.com>
2019-10-26 09:03:23 -0400

    woo

---
+++ build/precache-manifest.f3d219221a69bd92ef47b7371a138569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6dabf3613c5221e005e488773f15a71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345d51d3fdb7caefaecb",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "76a05847ff721a03b668",
+    "url": "/static/js/2.a19d10ab.chunk.js"
+  },
+  {
+    "revision": "345d51d3fdb7caefaecb",
+    "url": "/static/js/main.ffa2149e.chunk.js"
+  },
+  {
+    "revision": "39edffeab21473790da1",
+    "url": "/static/js/runtime-main.d2f9eeca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
